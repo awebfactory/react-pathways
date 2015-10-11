98a45e7d3db0a5ae55243ced64a8b0e8cebc648a
Victor Kane <victorkane@gmail.com>
2015-10-10 22:04:34 -0300

    added hard-coded favicon and logo

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="page-header awf-header">
-        <h1>Welcome to react-starter!</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container">
+                <div className="page-header">
+                    <img src="images/logo.png"/>
+                </div>
+                <div className="jumbotron awf-header">
+                    <h1>Welcome to react-starter!</h1>
+                </div>
+            </div>
+        );
+    }
 }
 
-ReactDOM.render(<App />, document.querySelector("#app"));
+ReactDOM.render(<App/>, document.querySelector("#app"));
