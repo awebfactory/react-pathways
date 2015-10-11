5bef17924473c0ebfef22e548fd0d7d560b59ed5
Victor Kane <victorkane@gmail.com>
2015-10-10 22:42:51 -0300

    moduralization of App component invoked by main ReactDOM renderer

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="page-header">
+                    <img src="images/logo.png"/>
+                </div>
+                <div className="jumbotron awf-header">
+                    <h1>Welcome to react-starter!</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,19 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="page-header">
-                    <img src="images/logo.png"/>
-                </div>
-                <div className="jumbotron awf-header">
-                    <h1>Welcome to react-starter!</h1>
-                </div>
-            </div>
-        );
-    }
-}
+import App from './components/app';
 
 ReactDOM.render(<App/>, document.querySelector("#app"));
