192fe02d25c333eecbe86158a43b123ffe13b8e7
victorkane <victorkane@gmail.com>
2015-10-16 18:25:54 -0300

    cleanup

--- src/components/app.js
+++ src/components/app.js
@@ -3,14 +3,11 @@ import Header from './home/header';
 
 class App extends React.Component {
     getDocs () {
-      return {
-
-            docs: [
+      return [
               { title: 'title 1', body: 'body 1' },
               { title: 'title 2', body: 'body 2' },
               { title: 'title 3', body: 'body 3' }
             ]
-      }
     }
     render() {
         let data = this.getDocs();
