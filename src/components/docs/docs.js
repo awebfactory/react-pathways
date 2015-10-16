9f38094814bed9ad8e2bdd2ee2424df2601291e5
victorkane <victorkane@gmail.com>
2015-10-16 19:09:06 -0300

    docs passed as prop to child components

--- src/components/docs/docs.js
+++ src/components/docs/docs.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 class Docs extends React.Component {
     render() {
+      console.log(this.props.docs);
         return (
             <div className="jumbotron awf-header">
                 <h1>react-starter Docs</h1>
