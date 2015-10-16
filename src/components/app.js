9a36f76aabe9793c4063d8147916a3752cf92185
victorkane <victorkane@gmail.com>
2015-10-16 18:17:14 -0300

    props receive data in Docs

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,9 @@ import Header from './home/header';
 
 class App extends React.Component {
     componentDidMount() {
-      this.state = {};
+      if (typeof state === "undefined") {
+        this.state = {};
+      }
       this.state.data = {
             docs: [
               { title: 'title 1', body: 'body 1' },
@@ -12,11 +14,22 @@ class App extends React.Component {
             ]
       }
     }
+    theDocs () {
+      return {
+
+            docs: [
+              { title: 'title 1', body: 'body 1' },
+              { title: 'title 2', body: 'body 2' },
+              { title: 'title 3', body: 'body 3' }
+            ]
+      }
+    }
     render() {
+        let data = this.theDocs();
         return (
             <div className="container">
                 <Header />
-                {this.props.children}
+                {React.cloneElement(this.props.children, {docs: {data}})}
             </div>
         );
     }
