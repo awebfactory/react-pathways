706ce22d9a412e4dfa50de8fbcbd8e07098602db
victorkane <victorkane@gmail.com>
2015-10-16 16:52:48 -0300

    added state data object to App component

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,16 @@ import React from 'react';
 import Header from './home/header';
 
 class App extends React.Component {
+    componentDidMount() {
+      this.state = {};
+      this.state.data = {
+            docs: [
+              { title: 'title 1', body: 'body 1' },
+              { title: 'title 2', body: 'body 2' },
+              { title: 'title 3', body: 'body 3' }
+            ]
+      }
+    }
     render() {
         return (
             <div className="container">
