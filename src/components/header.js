9b6832489e111c29be35bb99830813547e405c32
Victor Kane <victorkane@gmail.com>
2015-12-05 09:03:16 -0500

    initial styling navbar

--- src/components/header.js
+++ src/components/header.js
@@ -4,11 +4,13 @@ import { Link, IndexLink } from 'react-router';
 const Logo = () => <div className="logo"><Link to="/"><img src="images/logo.png" /></Link></div> 
 const Nav = () => {
 	return (
-	    <nav>
-	        <IndexLink to="/">Home</IndexLink>
-            <Link to='/about'>About</Link>
-            <Link to='/docs'>Docs</Link>
-            <Link to='/contact'>Contact</Link>
+	    <nav className="navbar navbar-inverse">
+	        <ul className="nav navbar-nav">
+	            <li><IndexLink to="/">Home</IndexLink></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/docs'>Docs</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
+            </ul>
 	    </nav>
     )
 }
