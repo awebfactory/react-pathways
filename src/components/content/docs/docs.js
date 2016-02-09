2be8781f67e3b44e6520f724070f50a4cc942a9d
Victor Kane <victorkane@gmail.com>
2016-02-09 12:45:56 -0500

    Stubbed out DocsList component and default render for empty docsList array

--- src/components/content/docs/docs.js
+++ src/components/content/docs/docs.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import DocsList from './docs-list';
 
 class Docs extends React.Component {
+    constructor(){
+	    super();
+		this.state = {
+		    docs: []
+		}
+    }
     render() {
+        const docListComponent = this.state.docs.length !== 0 ? 
+                        <DocsList docs={this.state.docs}/> :
+                        <span><h3>Click on a doc to view</h3><h4>(No docs yet! Create some docs!)</h4></span>
+    	
         return (
-            <div className="jumbotron awf-header">
-                <h1>Docs react-starter</h1>
+            <div className="container">
+            	<div className="jumbotron awf-header">
+            	    <h1>Docs react-starter</h1>
+            	</div>
+            	<div className="row">
+                    <div className="sidebar col-sm-6">
+                        {docListComponent}
+                    </div>
+            	</div>
             </div>
         );
     }
