ca7416e715fdb86a63fafd21223112c01fea790c
Victor Kane <victorkane@gmail.com>
2016-02-09 22:30:11 -0500

    stubbed out CurrentDoc

---
+++ src/components/content/docs/current-doc.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class CurrentDoc extends React.Component {
+	render() {
+		return (
+			<div><p>hola chicos</p></div>
+		);
+	}
+}
--- src/components/content/docs/docs.js
+++ src/components/content/docs/docs.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import DocsList from './docs-list';
+import CurrentDoc from './current-doc';
 import docUtils from '../../../helpers/doc-utils';
 
 class Docs extends React.Component {
     constructor(){
 	    super();
 		this.state = {
-		    docs: []
+		    docs: [],
+		    currentDoc: ''
 		}
     }
     init() {
@@ -26,7 +28,11 @@ class Docs extends React.Component {
         const docListComponent = this.state.docs.length !== 0 ? 
                         <DocsList docs={this.state.docs}/> :
                         <span><h3>Click on a doc to view</h3><h4>(No docs yet! Create some docs!)</h4></span>
-    	
+        const currentDoc = this.state.currentDoc !== '' ? 
+                        <CurrentDoc doc={this.state.currentDoc} /> :
+                        <div className="page-header awf-header">
+                            <h2>'Docs. Read all the things.'</h2>
+                        </div>
         return (
             <div className="container">
             	<div className="jumbotron awf-header">
@@ -35,6 +41,9 @@ class Docs extends React.Component {
             	<div className="row">
                     <div className="sidebar col-sm-6">
                         {docListComponent}
+                    </div>
+                    <div className="content col-sm-6">
+                        {currentDoc}
                     </div>
             	</div>
             </div>
