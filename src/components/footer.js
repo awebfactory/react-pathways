9457a664cb26fcea58ef895fa2d783bb006dd052
Victor Kane <victorkane@gmail.com>
2016-04-11 19:51:26 +0000

    Added social networking icons for footer for #5

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,8 +12,8 @@ class Footer extends React.Component {
             Victor Kane and <a href="http://awebfactory.com" _target="_blank"><img title="AWebFactory.com" src="images/awf-favicon.ico" width="32" height="32" /></a>
           </p>
           <p className="pull-right">
-            <a href="https://twitter.com/ReactPathways" target="_blank">Twitter <span className="glyphicon glyphicon-star" aria-hidden="true"></span></a>
-            <a href="https://www.facebook.com/ReactPathways-1607302469593834/" target="_blank">Facebook <span className="glyphicon glyphicon-search" aria-hidden="true"></span></a>
+            <a href="https://twitter.com/ReactPathways" target="_blank"><img name="Twitter" src="images/icons/twitter-logo-button.png" width="32" height="32"/></a>
+            <a href="https://www.facebook.com/ReactPathways-1607302469593834/" target="_blank"><img name="Twitter" src="images/icons/facebook-logo-button.png" width="32" height="32" /></a>
           </p>
         </div>
         </div>
