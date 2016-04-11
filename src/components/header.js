274580fa38e6944e71e61c3b86458a7177d37be7
Victor Kane <victorkane@gmail.com>
2016-04-11 03:01:05 +0000

    menu collapsible on device sensitive basis, styled and completed branding for mvp01 for #15

--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,32 @@ const Logo = () => <div className="nav-home logo"><Link to="/"><img src="images/
 const Nav = () => {
 	return (
         <nav className="navbar navbar-default navbar-fixed-top">
-            <ul className="nav navbar-nav">
-                <li><Logo /></li>
-                <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
-                <li><Link to='about' activeClassName="active">About</Link></li>
-                <li><Link to='docs' activeClassName="active">Docs</Link></li>
-                <li><Link to='contact' activeClassName="active">Contact</Link></li>
-            </ul>
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#rf-navbar">
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <Logo />
+                </div>
+                <div className="collapse navbar-collapse" id="rf-navbar">
+                    <ul className="nav navbar-nav">
+                        <li>
+                            <IndexLink to="/" activeClassName="active">Home</IndexLink>
+                        </li>
+                        <li>
+                            <Link to='about' activeClassName="active">About</Link>
+                        </li>
+                        <li>
+                            <Link to='docs' activeClassName="active">Docs</Link>
+                        </li>
+                        <li>
+                            <Link to='contact' activeClassName="active">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     )
 }
