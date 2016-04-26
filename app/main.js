619ef3803a7c1235b304aadb9edbfe7ae22cdc7e
Victor Kane <victorkane@gmail.com>
2016-04-26 13:17:26 +0000

    simplified broswerHistory

--- app/main.js
+++ app/main.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createHistory } from 'history';
-import { Router, useRouterHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-
-const browserHistory = useRouterHistory(createHistory)({
-            basename: '/'
-        });
 
 ReactDOM.render((
   <Router history={browserHistory} routes={routes} />
