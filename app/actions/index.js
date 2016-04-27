5ac473b499feeb6dca28ff3b077dcd514bafb1b3
Victor Kane <victorkane@gmail.com>
2016-04-27 23:06:59 +0000

    completed synchronous action creators and reducers

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,5 +1,21 @@
 export const REQUEST_STEPS = 'REQUEST_STEPS'
 export const RECEIVE_STEPS = 'RECEIVE_STEPS'
+export const SELECT_PATH = 'SELECT_PATH'
+export const INVALIDATE_PATH = 'INVALIDATE_PATH'
+
+export function selectPath(path) {
+  return {
+    type: SELECT_PATH,
+    path
+  }
+}
+
+export function invalidatePath(path) {
+  return {
+    type: INVALIDATE_PATH,
+    path
+  }
+}
 
 export const requestSteps = (selectedPath = "basicDefault") => {
     return {
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,15 +1,61 @@
+import { combineReducers } from 'redux'
 import {
-    REQUEST_STEPS, RECEIVE_STEPS
+  SELECT_PATH, INVALIDATE_PATH,
+  REQUEST_STEPS, RECEIVE_STEPS
 } from '../actions'
 
-function paths(state = [], action) {
-    switch (action.type) {
-        case REQUEST_STEPS:
-        case RECEIVE_STEPS:
-            return state
-        default:
-            return state
-   } 
+function selectedPath(state = 'basicDefault', action) {
+  switch (action.type) {
+    case SELECT_PATH:
+      return action.path
+    default:
+      return state
+  }
 }
 
-export default paths
+function steps(state = {
+  isFetching: false,
+  didInvalidate: false,
+  items: []
+}, action) {
+  switch (action.type) {
+    case INVALIDATE_PATH:
+      return Object.assign({}, state, {
+        didInvalidate: true
+      })
+    case REQUEST_STEPS:
+      return Object.assign({}, state, {
+        isFetching: true,
+        didInvalidate: false
+      })
+    case RECEIVE_STEPS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: false,
+        items: action.steps,
+        lastUpdated: action.receivedAt
+      })
+    default:
+      return state
+  }
+}
+
+function stepsByPath(state = { }, action) {
+  switch (action.type) {
+    case INVALIDATE_PATH:
+    case RECEIVE_STEPS:
+    case REQUEST_STEPS:
+      return Object.assign({}, state, {
+        [action.path]: steps(state[action.path], action)
+      })
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  stepsByPath,
+  selectedPath
+})
+
+export default rootReducer
