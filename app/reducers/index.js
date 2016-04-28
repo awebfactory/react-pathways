7ff557121ed240bff7fd53409df4269d41824b86
Victor Kane <victorkane@gmail.com>
2016-04-28 19:23:49 +0000

    modularized path reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,55 +1,5 @@
 import { combineReducers } from 'redux'
-import {
-  SELECT_PATH, INVALIDATE_PATH,
-  REQUEST_STEPS, RECEIVE_STEPS
-} from '../actions'
-
-function selectedPath(state = 'basicDefault', action) {
-  switch (action.type) {
-    case SELECT_PATH:
-      return action.path
-    default:
-      return state
-  }
-}
-
-function steps(state = {
-  isFetching: false,
-  didInvalidate: false,
-  items: []
-}, action) {
-  switch (action.type) {
-    case INVALIDATE_PATH:
-      return Object.assign({}, state, {
-        didInvalidate: true
-      })
-    case REQUEST_STEPS:
-      return Object.assign({}, state, {
-        isFetching: true,
-      })
-    case RECEIVE_STEPS:
-      return Object.assign({}, state, {
-        isFetching: false,
-        items: action.steps,
-        lastUpdated: action.receivedAt
-      })
-    default:
-      return state
-  }
-}
-
-function stepsByPath(state = { }, action) {
-  switch (action.type) {
-    case INVALIDATE_PATH:
-    case RECEIVE_STEPS:
-    case REQUEST_STEPS:
-      return Object.assign({}, state, {
-        [action.selectedPath]: steps(state[action.selectedPath], action)
-      })
-    default:
-      return state
-  }
-}
+import { stepsByPath, selectedPath } from './paths'
 
 const rootReducer = combineReducers({
   stepsByPath,
