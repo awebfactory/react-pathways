e444deb368024c140aafc7098f65d71e5adf92ab
Victor Kane <victorkane@gmail.com>
2016-04-30 15:04:57 +0000

    removed fork me on gh diagonal and added octicon icon to footer for repo

--- app/components/footer.js
+++ app/components/footer.js
@@ -12,8 +12,9 @@ class Footer extends React.Component {
             Victor Kane and <a href="http://awebfactory.com" _target="_blank"><img title="AWebFactory.com" src="images/awf-favicon.ico" width="32" height="32" /></a>
           </p>
           <p className="pull-right">
-            <a className="sn" href="https://twitter.com/ReactPathways" target="_blank"><span className="flaticon-twitter-logo-button"/></a>
+            <a className="sn" href="https://twitter.com/a_web_factory" target="_blank"><span className="flaticon-twitter-logo-button"/></a>
             <a className="sn" nhref="https://www.facebook.com/ReactPathways-1607302469593834/" target="_blank"><span className="flaticon-facebook-logo-button" width="32" height="32" /></a>
+            <a className="sn sn-gh" href="https://github.com/awebfactory/react-pathways" target="_blank"><span className="mega-octicon octicon-mark-github" width="32" height="32" /></a>
           </p>
         </div>
         </div>
