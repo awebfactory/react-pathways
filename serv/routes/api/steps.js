557d61cbdee3f63a571c0b5fba12e21deec92a79
Victor Kane <victorkane@gmail.com>
2016-05-02 02:55:03 +0000

    implemented get step by mongo id

--- serv/routes/api/steps.js
+++ serv/routes/api/steps.js
@@ -14,4 +14,14 @@ router.get('/step', function(req, res) {
     })
 })
 
+router.get('/step/:_id', function(req, res) {
+    Step.findOne({_id: req.params._id}, function(err, step) {
+        if (err)
+            return res.json({error: "Error fetching step"});        
+        else if (!step)
+            return res.json({error: "Error finding step"});
+        res.send(step);
+    })
+})
+
 module.exports = router
