2975897a5b68054c1d4265c310ff32d7d80bcab6
Victor Kane <victorkane@gmail.com>
2016-05-04 16:21:19 +0000

    implemented server: user sign in

--- serv/routes/api/users.js
+++ serv/routes/api/users.js
@@ -2,6 +2,7 @@ var router = require('express').Router()
 var jwt     = require('jsonwebtoken')
 var bcrypt  = require('bcrypt')
 
+var config = require('../../../config')
 var User = require('../../models/user').User
 
 // GET ./api/user
@@ -25,7 +26,7 @@ router.post('/user/register', function(req, res, next) {
     const password = req.body.password
     const splitmail = email.split("@")
     const username = splitmail[0]
-    console.log('username, email, password', username, email, password, '\n')
+    //console.log('username, email, password', username, email, password, '\n')
     bcrypt.hash(password, 10, function (err, hash) {
         if (err) { next(err) }
         var user = new User({
@@ -43,6 +44,21 @@ router.post('/user/register', function(req, res, next) {
 })
 
 // POST ./api/user/signin
+router.post('/user/signin', function(req, res, next) {
+    // we could check for existence of username first, and use that if no email
+    const email = req.body.email
+    const password = req.body.password
+    User.findOne({email: email}, function(err, user) {
+        if (err) { return next(err) }
+        if (!user) { return res.sendStatus(401) }
+        bcrypt.compare(password, user.login.password, function (err, valid) {
+            if (err) { return next(err) }
+            if (!valid) { return res.sendStatus(401) }
+            var token = jwt.sign({email: email}, config.secretisimo)
+            res.json(token)
+        })
+    })
+})
 
 // Update account, profile
 // PUT ./api/user/update
