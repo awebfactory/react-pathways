3131b1fee82790f206b5216b79914e97ef0cf765
Victor Kane <victorkane@gmail.com>
2016-05-04 15:34:40 +0000

    implemented server: user register

--- serv/routes/api/users.js
+++ serv/routes/api/users.js
@@ -1,4 +1,6 @@
 var router = require('express').Router()
+var jwt     = require('jsonwebtoken')
+var bcrypt  = require('bcrypt')
 
 var User = require('../../models/user').User
 
@@ -13,5 +15,38 @@ router.get('/user', function(req, res) {
         res.send(users);
     })
 })
+
+// GET ./api/user/:id
+
+// POST ./api/user/register
+router.post('/user/register', function(req, res, next) {
+    //console.log('register user: ' + req.body)
+    const email = req.body.email
+    const password = req.body.password
+    const splitmail = email.split("@")
+    const username = splitmail[0]
+    console.log('username, email, password', username, email, password, '\n')
+    bcrypt.hash(password, 10, function (err, hash) {
+        if (err) { next(err) }
+        var user = new User({
+            email: email,
+            login: {
+                username: username,
+                password: hash
+            }
+        })
+        user.save(function (err) {
+            if (err) { next(err) }
+            res.sendStatus(201)
+        })
+    })
+})
+
+// POST ./api/user/signin
+
+// Update account, profile
+// PUT ./api/user/update
+
+// DELETE ./api/user/delete
 
 module.exports = router
